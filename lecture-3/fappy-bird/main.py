d3d0cbbba90167a178ffc858ba5d8466fea095e7
NAPAT <barnrang@gmail.com>
2020-06-14 15:56:14 +0900

    add comment

--- lecture-3/fappy-bird/main.py
+++ lecture-3/fappy-bird/main.py
@@ -1,4 +1,5 @@
 import pygame
+# Initialize pygame
 pygame.init()
 
 # import our files
@@ -8,35 +9,50 @@ from pipes import Pipe, Pipes
 from monitor import Monitor
 import debugger
 
+# Set game window the config width and height
 game_display = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
 pygame.display.set_caption('Flappy bird')
 
 clock = pygame.time.Clock()
+
+# Contain state
 game_quit = False
 space_press = False
 count = 0
 
 while not game_quit:
 
+    # Catch input from user
     for event in pygame.event.get():
+
+        # If user press exit
         if event.type == pygame.QUIT:
             game_quit = True
 
+        # If user press down a key
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 space_press = True
 
+        # If user release a key
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
                 space_press = False
 
         print(event)
-    # Display Contents
+
+    # Display Black Screen
     game_display.fill(config.BLACK)
+
+    # Display message if space is being pressed
+    # See more in debugger.py
     if space_press:
         debugger.show_key_press(game_display, "space")
 
+    # Update display after render
     pygame.display.update()
+    count = (count + 1) % (2 * config.FPS)
+
+    # For control constant Frames Per Second
     clock.tick(config.FPS)
 
-    count = (count + 1) % (2 * config.FPS)
